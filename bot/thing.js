6ec192bf86a69b227aa38a5117d310d381815567
NoBusinessDoingWithYouAtAllMate <66976417+NoBusinessDoingWithYouAtAllMate@users.noreply.github.com>
2020-07-17 10:52:10 +0100

    Update thing.js

--- bot/thing.js
+++ bot/thing.js
@@ -1001,6 +1001,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
 		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
 		try {
 			await reaction.fetch();
+                        await reaction.message.fetch();
 		} catch (error) {
 			console.log('Something went wrong when fetching the message: ', error);
 			// Return as `reaction.message.author` may be undefined/null
