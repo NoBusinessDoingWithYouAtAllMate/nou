7c457ef173b0bb7266f7fabfd32d65226de867a7
NoBusinessDoingWithYouAtAllMate <66976417+NoBusinessDoingWithYouAtAllMate@users.noreply.github.com>
2020-07-18 00:20:45 +0100

    Update thing.js

--- bot/thing.js
+++ bot/thing.js
@@ -810,15 +810,7 @@ m.user.send("You did not verify please rejoin the server, " + dataBase.getData("
 
 client.on("messageUpdate",(oldm,newm)=>{
 
-oldm.isOwner = function(){
-let e = false;
-if(this.id == owner.id || this.id == Zhekoay.id || this.id == "476110417035395082"){
-e = true;
-};
-return e;
-};
-
-if(oldm.member.isOwner() || oldm.author.bot) return;
+if(oldm.isOwner() || oldm.author.bot) return;
 let oldmc = "**" + oldm.toString() + "**";
 oldmc = oldmc.replace("@" + "everyone","@EVERYONE");
 oldmc = oldmc.replace("@" + "here","@HERE");
